0949108673ac553ec892ec34e6f8d5cf6bb1f9f3
Ali Parsai <ali.parsai@email.com>
2015-10-25 12:42:41 +0100

    7/9 operators adapted

--- JavaMutate.py
+++ JavaMutate.py
@@ -927,22 +927,157 @@ class JavaMutate(object):
     def conditionalOperatorInsertion(self, tree):   # covered by negateConditionals, both generate too many mutations
         pass
 
-    def shiftOperatorReplacement(self, tree):
+    def shiftOperatorReplacement(self, tree, mode="return_text", nodeIndex=None):
         assert isinstance(tree, JavaParser.CompilationUnitContext)
-        mutatedTreesTexts = list()
-        expressionList = self.javaParseObject.seek(tree, JavaParser.ExpressionContext)
 
-        while len(expressionList) > 0:
-            #tmpTree = copy.deepcopy(tree)
-            expressionIndex = expressionList.pop()
+        if mode == "return_text":
 
-            node = self.javaParseObject.getNode(tree, expressionIndex)
+            mutatedTreesTexts = list()
+            expressionList = self.javaParseObject.seek(tree, JavaParser.ExpressionContext)
+
+            while len(expressionList) > 0:
+                #tmpTree = copy.deepcopy(tree)
+                expressionIndex = expressionList.pop()
+
+                node = self.javaParseObject.getNode(tree, expressionIndex)
+                assert isinstance(node, JavaParser.ExpressionContext)
+
+                try:
+                    if (isinstance(node.children[0], JavaParser.ExpressionContext) and isinstance(node.children[1],
+                                                                                              TerminalNodeImpl) and isinstance(
+                        node.children[2], TerminalNodeImpl) and isinstance(node.children[3],
+                                                                           JavaParser.ExpressionContext)):
+                        threeTerminals = False
+                    elif (isinstance(node.children[0], JavaParser.ExpressionContext) and isinstance(node.children[1],
+                                                                                                TerminalNodeImpl) and isinstance(
+                            node.children[2], TerminalNodeImpl) and isinstance(node.children[3],
+                                                                           TerminalNodeImpl) and isinstance(
+                            node.children[4], JavaParser.ExpressionContext)):
+                        threeTerminals = True
+                    else:
+                        continue  # not a binary shift expression
+                except Exception, e:
+                    continue
+
+                try:
+                    if (threeTerminals is False) and (
+                            (node.children[1].symbol.text == u"<" and node.children[2].symbol.text == u"<") or (
+                                        node.children[1].symbol.text == u">" and node.children[2].symbol.text == u">")):
+                        pass
+
+                    elif (threeTerminals is True) and (
+                                    node.children[1].symbol.text == u">" and node.children[2].symbol.text == u">" and
+                                node.children[3].symbol.text == u">"):
+                        pass
+
+                    else:
+                        continue  # not a shift operator
+
+                except Exception, e:
+                    continue
+
+                mutationBefore = "----> before: " + node.getText()
+                if self.verbose:
+                    print mutationBefore
+
+                if threeTerminals:
+                    originalText1 = copy.deepcopy(node.children[1].symbol.text)
+                    originalText2 = copy.deepcopy(node.children[2].symbol.text)
+                    originalText3 = copy.deepcopy(node.children[3].symbol.text)
+
+                    if originalText1 == u">" and originalText2 == u">" and originalText3 == u">":
+                        node.children[3].symbol.text = u" "
+                    else:
+                        assert False
+
+                else:
+                    originalText1 = copy.deepcopy(node.children[1].symbol.text)
+                    originalText2 = copy.deepcopy(node.children[2].symbol.text)
+
+                    if originalText1 == u">" and originalText2 == u">":
+                        node.children[1].symbol.text = u"<"
+                        node.children[2].symbol.text = u"<"
+                    elif originalText1 == u"<" and originalText2 == u"<":
+                        node.children[1].symbol.text = u">"
+                        node.children[2].symbol.text = u">"
+                    else:
+                        assert False
+
+                mutationAfter = "----> after: " + node.getText()
+                if self.verbose:
+                    print mutationAfter
+
+                mutatedTreesTexts.append((
+                    "/* LittleDarwin generated mutant\n mutant type: shiftOperatorReplacement\n " + mutationBefore + "\n" + mutationAfter + "\n----> line number in original file: " + str(node.start.line) + "\n*/ \n\n" + (
+                    " ".join(tree.getText().rsplit("<EOF>", 1)))))  # create compilable, readable code
+
+                node.children[1].symbol.text = copy.deepcopy(originalText1)
+                node.children[2].symbol.text = copy.deepcopy(originalText2)
+                if threeTerminals:
+                    node.children[3].symbol.text = copy.deepcopy(originalText3)
+
+            return mutatedTreesTexts
+
+        elif mode == "return_nodes":
+
+            nodeList = list()
+            expressionList = self.javaParseObject.seek(tree, JavaParser.ExpressionContext)
+
+            while len(expressionList) > 0:
+                #tmpTree = copy.deepcopy(tree)
+                expressionIndex = expressionList.pop()
+
+                node = self.javaParseObject.getNode(tree, expressionIndex)
+                assert isinstance(node, JavaParser.ExpressionContext)
+
+                try:
+                    if (isinstance(node.children[0], JavaParser.ExpressionContext) and isinstance(node.children[1],
+                                                                                              TerminalNodeImpl) and isinstance(
+                        node.children[2], TerminalNodeImpl) and isinstance(node.children[3],
+                                                                           JavaParser.ExpressionContext)):
+                        threeTerminals = False
+                    elif (isinstance(node.children[0], JavaParser.ExpressionContext) and isinstance(node.children[1],
+                                                                                                TerminalNodeImpl) and isinstance(
+                            node.children[2], TerminalNodeImpl) and isinstance(node.children[3],
+                                                                           TerminalNodeImpl) and isinstance(
+                            node.children[4], JavaParser.ExpressionContext)):
+                        threeTerminals = True
+                    else:
+                        continue  # not a binary shift expression
+                except Exception, e:
+                    continue
+
+                try:
+                    if (threeTerminals is False) and (
+                            (node.children[1].symbol.text == u"<" and node.children[2].symbol.text == u"<") or (
+                                        node.children[1].symbol.text == u">" and node.children[2].symbol.text == u">")):
+                        pass
+
+                    elif (threeTerminals is True) and (
+                                    node.children[1].symbol.text == u">" and node.children[2].symbol.text == u">" and
+                                node.children[3].symbol.text == u">"):
+                        pass
+
+                    else:
+                        continue  # not a shift operator
+
+                except Exception, e:
+                    continue
+
+                nodeList.append([expressionIndex, "ShiftOperatorReplacement"])
+
+            return nodeList
+
+        if mode == "return_tree":
+            assert nodeIndex is not None
+
+            node = self.javaParseObject.getNode(tree, nodeIndex)
             assert isinstance(node, JavaParser.ExpressionContext)
 
             try:
                 if (isinstance(node.children[0], JavaParser.ExpressionContext) and isinstance(node.children[1],
                                                                                               TerminalNodeImpl) and isinstance(
-                        node.children[2], TerminalNodeImpl) and isinstance(node.children[3],
+                    node.children[2], TerminalNodeImpl) and isinstance(node.children[3],
                                                                            JavaParser.ExpressionContext)):
                     threeTerminals = False
                 elif (isinstance(node.children[0], JavaParser.ExpressionContext) and isinstance(node.children[1],
@@ -951,33 +1086,33 @@ class JavaMutate(object):
                                                                            TerminalNodeImpl) and isinstance(
                         node.children[4], JavaParser.ExpressionContext)):
                     threeTerminals = True
+
                 else:
-                    continue  # not a binary shift expression
+                    assert False
             except Exception, e:
-                continue
+                assert False
 
             try:
                 if (threeTerminals is False) and (
-                            (node.children[1].symbol.text == u"<" and node.children[2].symbol.text == u"<") or (
-                                        node.children[1].symbol.text == u">" and node.children[2].symbol.text == u">")):
+                        (node.children[1].symbol.text == u"<" and node.children[2].symbol.text == u"<") or (
+                                    node.children[1].symbol.text == u">" and node.children[2].symbol.text == u">")):
                     pass
-                elif (threeTerminals is True) and (
-                                    node.children[1].symbol.text == u">" and node.children[2].symbol.text == u">" and
-                                node.children[3].symbol.text == u">"):
-                    pass
-                else:
-                    continue  # not a shift operator
-            except Exception, e:
-                continue
 
-            mutationBefore = "----> before: " + node.getText()
-            if self.verbose:
-                print mutationBefore
+                elif (threeTerminals is True) and (
+                                node.children[1].symbol.text == u">" and node.children[2].symbol.text == u">" and
+                            node.children[3].symbol.text == u">"):
+                    pass
+
+                else:
+                    assert False
+
+            except Exception, e:
+                assert False
 
             if threeTerminals:
-                originalText1 = copy.deepcopy(node.children[1].symbol.text)
-                originalText2 = copy.deepcopy(node.children[2].symbol.text)
-                originalText3 = copy.deepcopy(node.children[3].symbol.text)
+                originalText1 = node.children[1].symbol.text
+                originalText2 = node.children[2].symbol.text
+                originalText3 = node.children[3].symbol.text
 
                 if originalText1 == u">" and originalText2 == u">" and originalText3 == u">":
                     node.children[3].symbol.text = u" "
@@ -985,8 +1120,8 @@ class JavaMutate(object):
                     assert False
 
             else:
-                originalText1 = copy.deepcopy(node.children[1].symbol.text)
-                originalText2 = copy.deepcopy(node.children[2].symbol.text)
+                originalText1 = node.children[1].symbol.text
+                originalText2 = node.children[2].symbol.text
 
                 if originalText1 == u">" and originalText2 == u">":
                     node.children[1].symbol.text = u"<"
@@ -997,20 +1132,10 @@ class JavaMutate(object):
                 else:
                     assert False
 
-            mutationAfter = "----> after: " + node.getText()
-            if self.verbose:
-                print mutationAfter
+            return tree
 
-            mutatedTreesTexts.append((
-                "/* LittleDarwin generated mutant\n mutant type: shiftOperatorReplacement\n " + mutationBefore + "\n" + mutationAfter + "\n----> line number in original file: " + str(node.start.line) + "\n*/ \n\n" + (
-                    " ".join(tree.getText().rsplit("<EOF>", 1)))))  # create compilable, readable code
 
-            node.children[1].symbol.text = copy.deepcopy(originalText1)
-            node.children[2].symbol.text = copy.deepcopy(originalText2)
-            if threeTerminals:
-                node.children[3].symbol.text = copy.deepcopy(originalText3)
 
-        return mutatedTreesTexts
 
     def logicalOperatorReplacement(self, tree):
         assert isinstance(tree, JavaParser.CompilationUnitContext)
