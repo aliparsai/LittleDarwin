5e3576ca3e4a28ca9d87f35f043234e2affc1785
Ali Parsai <ali.parsai@email.com>
2016-04-05 20:40:50 +0200

    wrong data corrected

--- HigherOrderExperiment/analyze2.R
+++ HigherOrderExperiment/analyze2.R
@@ -1,6 +1,6 @@
-fo = read.csv("analyze/fo-60.csv", header=F)
+fo = read.csv("analyze/fo-9.csv", header=F)
 fop=fo$V3/fo$V4
-to = read.csv("analyze/to-20.csv", header=F)
+to = read.csv("analyze/to-3.csv", header=F)
 top=to$V3/to$V4
 pdf("chart2.pdf")
 curve(1-(1-x)^3,0,1,col="red", xlab="First-Order Mutation Coverage", ylab="Third-Order Mutation Coverage")
