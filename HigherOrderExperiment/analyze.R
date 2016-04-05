574b777e0d73c2b02dcaa37970e7ee0a18ab4958
Ali Parsai <ali.parsai@email.com>
2016-04-05 09:00:17 +0200

    data

--- HigherOrderExperiment/analyze.R
+++ HigherOrderExperiment/analyze.R
@@ -1,6 +1,6 @@
-fo = read.csv("analyze/fo-14.csv", header=F)
+fo = read.csv("analyze/fo-10.csv", header=F)
 fop=fo$V3/fo$V4
-so = read.csv("analyze/so-7.csv", header=F)
+so = read.csv("analyze/so-5.csv", header=F)
 sop=so$V3/so$V4
 pdf("chart.pdf")
 curve(1-(1-x)^2,0,1,col="red", xlab="First-Order Mutation Coverage", ylab="Second-Order Mutation Coverage")
