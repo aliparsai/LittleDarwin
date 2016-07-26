081eca3e8b6ae88cc2b65800ec2f8c52131ba7aa
Ali Parsai <ali.parsai@email.com>
2016-07-26 13:44:32 +0200

    added cloverDB Support

---
+++ SubsumingExperiment/RetrieveLineCoverageFromCloverDB.py
@@ -0,0 +1,45 @@
+import os
+from subprocess import PIPE, Popen
+
+
+class CloverDBParser(object):
+    def __init__(self, dbFile=None, jarFile=None):
+        self.cloverDB = dbFile
+        self.javaCloverDBHandler = jarFile
+
+    def jarWrapper(self, args):
+        assert isinstance(args, list)
+        stringArgs = ['java', '-jar']
+        stringArgs.extend([str(x) for x in args])
+        process = Popen(stringArgs, stdout=PIPE, stderr=PIPE)
+        returnResults = list()
+        while process.poll() is None:
+            line = process.stdout.readline()
+            if line != '' and line.endswith('\n'):
+                returnResults.append(line[:-1])
+        stdout, stderr = process.communicate()
+        returnResults += stdout.split('\n')
+        if stderr != '':
+            returnResults += stderr.split('\n')
+        returnResults.remove('')
+        return returnResults
+
+    def getResults(self, filePath, lineNumber):
+        assert os.path.exists(self.cloverDB)
+        assert os.path.exists(self.javaCloverDBHandler)
+        assert isinstance(lineNumber, int)
+        result = self.jarWrapper([self.javaCloverDBHandler, self.cloverDB, filePath, lineNumber])
+        return result
+
+    def findTestCases(self, filePath, lineNumber):
+        return self.getResults(filePath, lineNumber)[1:]
+
+    def findCoverage(self, filePath, lineNumber):
+        try:
+            result = int(self.getResults(filePath, lineNumber)[0])
+        except ValueError:
+            result = -1
+
+        return result
+
+
