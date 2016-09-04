f661d339e3bd7d4299a96d3251ce3967b8349398
Ali Parsai <ali.parsai@email.com>
2016-09-04 11:44:02 +0200

    broken 2

--- SubsumingExperiment/SubsumptionGraph.py
+++ SubsumingExperiment/SubsumptionGraph.py
@@ -14,11 +14,41 @@ class MutantSubsumptionGraphNode(object):
             return 1
         return 0
 
+    def checkSubsuming(self, node):
+        assert isinstance(node, MutantSubsumptionGraphNode)
+        if len(node.mutants) <= 0 or len(self.mutants) <= 0:
+            return 0
+
+        me = list(self.mutants)[0]
+        him = list(node.mutants)[0]
+
+        assert isinstance(me, Mutant)
+        assert isinstance(him, Mutant)
+
+        if me.failedTests < him.failedTests:
+            return True
+        return False
+
+
+class MutantSubsumptionGraphEdge(object):
+    newID = itertools.count().next
+    def __init__(self, subsuming=None, subsumed=None):
+        self.id = MutantSubsumptionGraphEdge.newID()
+        self.mutantSubsuming = subsuming
+        self.mutantSubsumed = subsumed
+
+    def __eq__(self, other):
+        assert isinstance(other, MutantSubsumptionGraphEdge)
+        if self.mutantSubsuming is other.mutantSubsuming and self.mutantSubsumed is other.mutantSubsumed:
+            return True
+        return False
+
+
 
 class MutantSubsumptionGraph(object):
     def __init__(self):
         self.nodes = list()
-        self.edges = dict()
+        self.edges = list()
 
     def addMutant(self, mutant):
         assert isinstance(mutant, Mutant)
@@ -33,4 +63,27 @@ class MutantSubsumptionGraph(object):
             newNode = MutantSubsumptionGraphNode()
             newNode.addMutant(mutant)
             self.nodes.append(newNode)
+
+        for node1 in self.nodes:
+            assert isinstance(node1, MutantSubsumptionGraphNode)
+
+            for node2 in self.nodes:
+                assert isinstance(node2, MutantSubsumptionGraphNode)
+
+                if node1 is node2:
+                    continue
+
+                if node1.checkSubsuming(node2):
+                    newEdge = MutantSubsumptionGraphEdge(node1, node2)
+                    exists = False
+
+                    for edge in self.edges:
+                        if newEdge == edge:
+                            exists = True
+                            break
+
+                    if not exists:
+                        self.edges.append(newEdge)
+
+
 
