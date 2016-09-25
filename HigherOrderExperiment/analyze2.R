7fa192c048842b65b708c8cce76ea720054f8f40
Ali Parsai <ali.parsai@email.com>
2016-09-26 00:42:10 +0200

    removing unneccessary files

--- HigherOrderExperiment/analyze2.R
+++
@@ -1,8 +0,0 @@
-fo = read.csv("analyze/fo-9.csv", header=F)
-fop=fo$V3/fo$V4
-to = read.csv("analyze/to-3.csv", header=F)
-top=to$V3/to$V4
-pdf("chart2.pdf")
-curve(1-(1-x)^3,0,1,col="red", xlab="First-Order Mutation Coverage", ylab="Third-Order Mutation Coverage")
-points(fop,top,pch=20,col="blue")
-dev.off()
