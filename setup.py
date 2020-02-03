35a3b6a46bf3cb4f79dfb75fafb87fb365d58ede
Ali Parsai <ali.parsai@email.com>
2020-02-03 14:47:36 +0100

    ready for packaging

--- setup.py
+++ setup.py
@@ -1,13 +1,25 @@
-from setuptools import setup
+import setuptools
 
-setup(
-    name='littledarwin',
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='littledarwin-aliparsai',
     version='0.5',
-    packages=['littledarwin', 'custom_antlr4', 'custom_antlr4.atn', 'custom_antlr4.dfa', 'custom_antlr4.tree',
-              'custom_antlr4.error', 'custom_antlr4.xpath'],
     url='https://littledarwin.parsai.net',
+    author="Ali Parsai",
+    author_email="ali.parsai@live.com",
+    description="LittleDarwin Mutation Analysis Framework",
+    long_description=long_description,
+    long_description_content_type="text/plain",
     license='GNU GPLv3',
-    author='Ali Parsai',
-    author_email='ali@parsai.net',
-    description='LittleDarwin Mutation Testing Tool'
+    packages=setuptools.find_packages(),
+#   packages=['littledarwin', 'custom_antlr4', 'custom_antlr4.atn', 'custom_antlr4.dfa', 'custom_antlr4.tree', 'custom_antlr4.error', 'custom_antlr4.xpath'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.3',
 )
+
