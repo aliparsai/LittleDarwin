b7e0bae11a3107024cb13d32be6d093fba1c1ac0
Ali Parsai <ali.parsai@email.com>
2020-02-08 11:49:35 +0100

    fixed stuff in comparereports

--- utils/MutantDensity/CompareReports.py
+++ utils/MutantDensity/CompareReports.py
@@ -6,28 +6,28 @@ for reportArg in sys.argv[1:]:
     reportFileHandleDict[reportName] = open(reportFile, 'r')
 
 reportValuesDict = dict()
+reportNames = sorted(list(reportFileHandleDict.keys()))
 
 
-for reportName in reportFileHandleDict.keys():
-    print(reportName)
+for reportName in reportNames:
     reportValuesDict[reportName] = dict()
     for line in reportFileHandleDict[reportName]:
         name, value = line.strip('\n').split(',')
         reportValuesDict[reportName][name] = value
 
 names = set()
-for reportName in reportFileHandleDict.keys():
+for reportName in reportNames:
     names.update(set(reportValuesDict[reportName].keys()))
 
 with open("result.csv", 'w') as result:
     result.write("Name")
-    for reportName in reportFileHandleDict.keys():
+    for reportName in reportNames:
         result.write(',' + reportName)
     result.write('\n')
 
     namesList = sorted(list(names))
     for name in namesList:
         result.write('.'.join(str(name).split('.java')[0].split('/')))
-        for reportName in reportFileHandleDict.keys():
-            result.write(',' + (reportValuesDict[reportName][name] if name in reportValuesDict[reportName].keys() else 'N/A'))
+        for reportName in reportNames:
+            result.write(',' + (reportValuesDict[reportName][name] if name in reportValuesDict[reportName].keys() else '0'))
         result.write('\n')
