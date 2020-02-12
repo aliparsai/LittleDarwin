da25f9d4afd1eafeab635d8ee1a229ae3922d0a3
Ali Parsai <ali.parsai@email.com>
2020-02-12 23:57:14 +0100

    updated license stub

--- littledarwin/LittleDarwin.py
+++ littledarwin/LittleDarwin.py
@@ -1,36 +1,32 @@
+##########################################################################################################
+##                                                                                                      ##
+##     __     _  __   __   __       ____                          _                                     ##
+##    / /    (_)/ /_ / /_ / /___   / __ \ ____ _ _____ _      __ (_)____                                ##
+##   / /    / // __// __// // _ \ / / / // __ `// ___/| | /| / // // __ \                               ##
+##  / /___ / // /_ / /_ / //  __// /_/ // /_/ // /    | |/ |/ // // / / /                               ##
+## /_____//_/ \__/ \__//_/ \___//_____/ \__,_//_/     |__/|__//_//_/ /_/                                ##
+##                                                                                                      ##
+## Copyright (c) 2014-2020 Ali Parsai                                                                   ##
+##                                                                                                      ##
+## This program is free software: you can redistribute it and/or modify it under the terms of           ##
+## the GNU General Public License as published by the Free Software Foundation, either version 3        ##
+## of the License, or (at your option) any later version.                                               ##
+##                                                                                                      ##
+## This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;            ##
+## without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.            ##
+## See the GNU General Public License for more details.                                                 ##
+##                                                                                                      ##
+## You should have received a copy of the GNU General Public License along with this program.           ##
+## If not, see <https://www.gnu.org/licenses/>.                                                         ##
+##                                                                                                      ##
+## Find me at:                                                                                          ##
+## https://www.parsai.net                                                                               ##
+##                                                                                                      ##
+##########################################################################################################
+
+
 from __future__ import print_function
 from __future__ import division
-
-"""
-    __     _  __   __   __       ____                          _
-   / /    (_)/ /_ / /_ / /___   / __ \ ____ _ _____ _      __ (_)____
-  / /    / // __// __// // _ \ / / / // __ `// ___/| | /| / // // __ \
- / /___ / // /_ / /_ / //  __// /_/ // /_/ // /    | |/ |/ // // / / /
-/_____//_/ \__/ \__//_/ \___//_____/ \__,_//_/     |__/|__//_//_/ /_/
-
-Copyright (c) 2014-2020 Ali Parsai
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
-Ali Parsai
-www.parsai.net
-ali@parsai.net
-
-"""
-
-# generic imports
 import io
 import sys
 import platform
@@ -115,7 +111,6 @@ def main():
 
 
 def mutationPhase(options, filterType, filterList, higherOrder):
-    assert options.isVerboseActive is not None
     # creating our module objects.
     javaRead = JavaRead(options.isVerboseActive)
     javaParse = JavaParse(options.isVerboseActive)
@@ -200,7 +195,8 @@ def mutationPhase(options, filterType, filterList, higherOrder):
 def buildPhase(options):
     # let's tell the user upfront that this may corrupt the source code.
     print("\n\n!!! CAUTION !!!")
-    print("code can be changed accidentally. use a backup version.\n")
+    print("Code can be changed accidentally. Create a backup first.\n")
+
     reportGenerator = ReportGenerator(littleDarwinVersion)
     if options.alternateDb == "***dummy***":
         databasePath = os.path.abspath(os.path.join(options.buildPath, "LittleDarwinResults", "mutationdatabase"))
